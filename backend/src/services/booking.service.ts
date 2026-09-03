9595cc3683b6be6da9a4d132d5b5e631bd4fe3c9
Wayne <mingyuewayne@gmail.com>
2026-09-04 09:00:20 +1000

    Fix time out in sending email

--- backend/src/services/booking.service.ts
+++ backend/src/services/booking.service.ts
@@ -87,6 +87,9 @@ const createTransporter = () => {
       user: process.env.SMTP_USER || '',
       pass: process.env.SMTP_PASS || '',
     },
+    tls: {
+      rejectUnauthorized: false,
+    },
   });
 };
 
